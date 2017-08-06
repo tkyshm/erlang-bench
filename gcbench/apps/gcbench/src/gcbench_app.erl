a37e90ab9cefba8166bf7983a419f8872f0c0bf5
tkyshm <tkyshm@gmail.com>
2017-08-06 20:44:51 +0900

    profileを追加

--- gcbench/apps/gcbench/src/gcbench_app.erl
+++ gcbench/apps/gcbench/src/gcbench_app.erl
@@ -7,12 +7,28 @@
 
 -behaviour(application).
 
+%% API
+-export([profile_output/0,
+         fprof_start/1
+        ]).
+
 %% Application callbacks
 -export([start/2, stop/1]).
 
 %%====================================================================
 %% API
 %%====================================================================
+-spec profile_output() -> ok.
+profile_output() ->
+    fprof:trace([stop]),
+    fprof:profile([]),
+    fprof:analyse([{dest, "gcbench.analyze"}]),
+    fprof:stop().
+
+-spec fprof_start(pid()) -> ok.
+fprof_start(Pid) ->
+    {ok, _Pid} = fprof:start(),
+    fprof:trace([start, {procs, Pid}]).
 
 start(_StartType, _StartArgs) ->
     gcbench_sup:start_link().
@@ -20,7 +36,3 @@ start(_StartType, _StartArgs) ->
 %%--------------------------------------------------------------------
 stop(_State) ->
     ok.
-
-%%====================================================================
-%% Internal functions
-%%====================================================================
